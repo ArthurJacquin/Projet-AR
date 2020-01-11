0b464487c14f0b92c402b947c5393284facea726
Margot <gotgot07@hotmail.fr>
2020-01-11 22:09:35 +0100

    Map fonctionne

---
+++ Assets/Script/MovementCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCharacter : MonoBehaviour
+{
+    [SerializeField]
+    private Joystick leftJoystick;
+    [SerializeField]
+    private float speed;
+
+    private void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(leftJoystick.Horizontal * speed,
+                                         0.0f,
+                                         leftJoystick.Vertical * speed);
+    }
+}
