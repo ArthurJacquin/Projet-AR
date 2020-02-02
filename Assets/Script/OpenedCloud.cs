64efe619387de214068a4aea508ba57b9637e7bc
Margot <gotgot07@hotmail.fr>
2020-02-02 16:46:56 +0100

    essai collider infructueux :(

---
+++ Assets/Script/OpenedCloud.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenedCloud : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+}
