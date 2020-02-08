1e8d19fcdcc720b9d95f3edcff097b92383b7dbc
ArthurJacquin <artuj38@free.fr>
2020-02-08 19:22:37 +0100

    script mouvements

--- Assets/Script/CharacterManager.cs
+++ Assets/Script/CharacterManager.cs
@@ -4,54 +4,17 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 playerInitialPosition;
-    
-    //Mouvement parameters
     [SerializeField]
     private Joystick leftJoystick;
     [SerializeField]
-    private CharacterController controller;
-    [SerializeField]
     private float speed;
     [SerializeField]
-    private Vector3 moveDir = Vector3.zero;
-    private Vector3 dis = Vector3.zero;
-    private Vector3 moveDirForward = Vector3.zero;
-    private Vector3 moveDirSide = Vector3.zero;
-    private float horizontalInput = 0.0f;
-    private float verticalInput = 0.0f;
+    private Rigidbody player;
 
-    private void Awake()
+    private void FixedUpdate()
     {
-        moveDir = Vector3.zero;
-        playerInitialPosition = player.transform.position;
-    }
-
-    void FixedUpdate()
-    {
-        horizontalInput = leftJoystick.Horizontal;
-        verticalInput = leftJoystick.Vertical;
-
-        moveDirForward = transform.forward * verticalInput;
-        moveDirSide = transform.right * horizontalInput;
-        moveDir += (moveDirForward + moveDirSide).normalized;
-
-        dis = moveDir * speed * Time.deltaTime;
-
-        MovementUpdate();
-    }
-
-    private void MovementUpdate()
-    {
-        if (isMoving())
-            controller.Move(dis);
-        else
-            moveDir = Vector3.zero;
-    }
-
-    private bool isMoving()
-    {
-        return horizontalInput != 0 || verticalInput != 0;
+        Vector3 direction = -Vector3.right * leftJoystick.Vertical + Vector3.forward * leftJoystick.Horizontal;
+        transform.rotation = Quaternion.LookRotation(direction);
+        player.AddForce(direction * speed * Time.fixedDeltaTime);
     }
 }
