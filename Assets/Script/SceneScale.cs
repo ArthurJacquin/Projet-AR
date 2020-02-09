a20dfac3cd69dccee61f02d571b965d00149edd5
ArthurJacquin <artuj38@free.fr>
2020-02-09 14:23:02 +0100

    Scale de la scene dans le cloud

---
+++ Assets/Script/SceneScale.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneScale : MonoBehaviour
+{
+    [SerializeField] private Transform scene;
+
+    public void ScaleScene(float scale)
+    {
+        scene.localScale = new Vector3(scale, scale, scale);
+    }
+}
