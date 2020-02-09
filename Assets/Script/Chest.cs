5675ffc390e16c3cd4f1f5fb64a7dc24ffff2b31
Attika <attika.mayou@gmail.com>
2020-02-09 13:58:12 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Script/Chest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    [SerializeField] private GameObject useFruitButton;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        useFruitButton.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        useFruitButton.SetActive(false);
+    }
+}
