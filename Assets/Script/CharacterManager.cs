20bc7cd0d148567639f4ad6025f184d65c3104a0
ArthurJacquin <artuj38@free.fr>
2020-02-09 19:48:00 +0100

    orientation du player

--- Assets/Script/CharacterManager.cs
+++ Assets/Script/CharacterManager.cs
@@ -18,7 +18,8 @@ public class CharacterManager : MonoBehaviour
         if(leftJoystick.Vertical != 0 && leftJoystick.Horizontal != 0)
         {
             Vector3 direction = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z).normalized * leftJoystick.Vertical + new Vector3(Camera.main.transform.right.x, 0, Camera.main.transform.right.z).normalized * leftJoystick.Horizontal;
-            transform.rotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.GetChild(0).rotation = Quaternion.LookRotation(direction, Vector3.up);
             player.MovePosition(player.position + direction * speed * Time.fixedDeltaTime);
             if(animator.GetBool("Move") == false)
                 footstepSound.Play();
--- Assets/Script/TakeLadder.cs
+++ Assets/Script/TakeLadder.cs
@@ -8,56 +8,53 @@ public class TakeLadder : MonoBehaviour
     [SerializeField]  public GameObject player;
     [SerializeField]  public GameObject playerAnimator;
     static public int col;
-    /*
-    public void OnTriggerEnter(Collider other)
-    {
-        useButton.SetActive(true);
-    }
-    public void OnTriggerExit(Collider other)
-    {
-        useButton.SetActive(false);
-    }
-    */
+
     public void climbLadder()
     {
-        StartCoroutine(waitForAnim(3f));
+        StartCoroutine(waitForAnim(3.5f));
     }
 
     private IEnumerator waitForAnim(float time)
     {
-        //Debug.Log("in coroutine" + col);
         if (col == 1)
         {
             player.transform.localPosition = new Vector3(-0.8f, 0.115f, -0.27f);
             player.transform.rotation = Quaternion.LookRotation(Vector3.right);
+            player.transform.GetChild(0).rotation = Quaternion.LookRotation(Vector3.right);
 
             anim.speed = 2f;
             anim.Play("Climbing");
 
             yield return new WaitForSeconds(time);
+            anim.Play("Idle");
             playerAnimator.transform.localPosition = new Vector3(0f, 0f, 0f);
             player.transform.localPosition = new Vector3(-0.6f, 0.2533f, -0.26f);
             player.transform.rotation = Quaternion.LookRotation(Vector3.right);
-            anim.Play("Idle");
+            player.transform.GetChild(0).rotation = Quaternion.LookRotation(Vector3.right);
+            
         }
         else if (col == 3)
         {
-            player.transform.localPosition = new Vector3(-0.646f, 0.259f, -0.061f);
+            player.transform.localPosition = new Vector3(-0.703f, 0.259f, -0.061f);
             player.transform.rotation = Quaternion.LookRotation(Vector3.right);
+            player.transform.GetChild(0).rotation = Quaternion.LookRotation(Vector3.right);
             anim.speed = 2f;
             anim.Play("ClimbingBack");
 
             yield return new WaitForSeconds(time);
+            anim.Play("Idle");
             playerAnimator.transform.localPosition = new Vector3(0f, 0f, 0f);
             player.transform.localPosition = new Vector3(-0.7946f, 0.115f, -0.0612f);
             player.transform.rotation = Quaternion.LookRotation(Vector3.right);
-            anim.Play("Idle");
+            player.transform.GetChild(0).rotation = Quaternion.LookRotation(Vector3.right);
+            
         }
 
         else if (col == 2)
         {
             player.transform.localPosition = new Vector3(0.9f, 0.265f, -1.45f);
             player.transform.rotation = Quaternion.LookRotation(Vector3.forward);
+            player.transform.GetChild(0).rotation = Quaternion.LookRotation(Vector3.forward);
             anim.speed = 2f;
             anim.Play("ClimbingBack");
 
@@ -66,11 +63,13 @@ public class TakeLadder : MonoBehaviour
             playerAnimator.transform.localPosition = new Vector3(0f, 0f, 0f);
             player.transform.localPosition = new Vector3(0.88f, 0.115f, -1.35f);
             player.transform.rotation = Quaternion.LookRotation(-Vector3.forward);
+            player.transform.GetChild(0).rotation = Quaternion.LookRotation(-Vector3.forward);
         }
         else if (col == 4)
         {
             player.transform.localPosition = new Vector3(0.675f, 0.1097f, -1.538f);
             player.transform.rotation = Quaternion.LookRotation(-Vector3.right);
+            player.transform.GetChild(0).rotation = Quaternion.LookRotation(-Vector3.right);
             anim.speed = 2f;
             anim.Play("Climbing");
             yield return new WaitForSeconds(time);
@@ -78,6 +77,7 @@ public class TakeLadder : MonoBehaviour
             playerAnimator.transform.localPosition = new Vector3(0f, 0f, 0f);
             player.transform.localPosition = new Vector3(0.846f, 0.2621f, -1.5353f);
             player.transform.rotation = Quaternion.LookRotation(-Vector3.right);
+            player.transform.GetChild(0).rotation = Quaternion.LookRotation(-Vector3.right);
         }
 
     }
